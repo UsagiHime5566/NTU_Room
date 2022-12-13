7e1b5d0dadb63f435326c6394c3e5ebf2a5c9950
nikki <moniqshiin@gmail.com>
2022-12-13 19:26:18 +0800

    多個撥放器完成

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -7,18 +7,11 @@ using SFB;
 
 public class MainManager : MonoBehaviour
 {
-    public RenderTexture screen1;
-    public VideoPlayer videoPlayer;
+    public List<VideoPlayer> videoPlayers;
 
     [Header("Auto Work")]
     public bool imidiatePlay = false;
-
     string _path;
-
-    void Start()
-    {
-        
-    }
 
     void OnGUI() {
         var guiScale = new Vector3(Screen.width / 800.0f, Screen.height / 600.0f, 1.0f);
@@ -28,30 +21,23 @@ public class MainManager : MonoBehaviour
         GUILayout.BeginHorizontal();
         GUILayout.Space(20);
         GUILayout.BeginVertical();
-
         // Open File Samples
+        if (GUILayout.Button("Open File Video 0")) {
+            var extensions = new [] {
+                new ExtensionFilter("Support Video Files", "mp4"),
+                new ExtensionFilter("All Files", "*" ),
+            };
+            var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+            WriteResult(result);
+            if(result.Length > 0){
+                Debug.Log("Success");
+                SetupVideo(0, result[0]);
+            }
+        }
 
-        // if (GUILayout.Button("Open File")) {
-        //     WriteResult(StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false));
-        // }
-        // GUILayout.Space(5);
-        // if (GUILayout.Button("Open File Async")) {
-        //     StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", "", false, (string[] paths) => { WriteResult(paths); });
-        // }
-        // GUILayout.Space(5);
-        // if (GUILayout.Button("Open File Multiple")) {
-        //     WriteResult(StandaloneFileBrowser.OpenFilePanel("Open File", "", "", true));
-        // }
-        // GUILayout.Space(5);
-        // if (GUILayout.Button("Open File Extension")) {
-        //     WriteResult(StandaloneFileBrowser.OpenFilePanel("Open File", "", "txt", true));
-        // }
-        // GUILayout.Space(5);
-        // if (GUILayout.Button("Open File Directory")) {
-        //     WriteResult(StandaloneFileBrowser.OpenFilePanel("Open File", Application.dataPath, "", true));
-        // }
-        // GUILayout.Space(5);
-        if (GUILayout.Button("Open File Filter")) {
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Open File Video 1")) {
             var extensions = new [] {
                 new ExtensionFilter("Support Video Files", "mp4"),
                 new ExtensionFilter("All Files", "*" ),
@@ -61,7 +47,55 @@ public class MainManager : MonoBehaviour
             if(result.Length > 0){
                 Debug.Log("Success");
 
-                SetupVideo(result[0]);
+                SetupVideo(1, result[0]);
+            }
+        }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Open File Video 2")) {
+            var extensions = new [] {
+                new ExtensionFilter("Support Video Files", "mp4"),
+                new ExtensionFilter("All Files", "*" ),
+            };
+            var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+            WriteResult(result);
+            if(result.Length > 0){
+                Debug.Log("Success");
+
+                SetupVideo(2, result[0]);
+            }
+        }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Open File Video 3")) {
+            var extensions = new [] {
+                new ExtensionFilter("Support Video Files", "mp4"),
+                new ExtensionFilter("All Files", "*" ),
+            };
+            var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+            WriteResult(result);
+            if(result.Length > 0){
+                Debug.Log("Success");
+
+                SetupVideo(3, result[0]);
+            }
+        }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Open File Video 4")) {
+            var extensions = new [] {
+                new ExtensionFilter("Support Video Files", "mp4"),
+                new ExtensionFilter("All Files", "*" ),
+            };
+            var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+            WriteResult(result);
+            if(result.Length > 0){
+                Debug.Log("Success");
+
+                SetupVideo(4, result[0]);
             }
         }
 
@@ -81,20 +115,20 @@ public class MainManager : MonoBehaviour
         }
     }
 
-    void SetupVideo(string filePath){
-        videoPlayer.url = filePath;
-        videoPlayer.Prepare();
-        videoPlayer.loopPointReached += delegate {
+    void SetupVideo(int index, string filePath){
+        videoPlayers[index].url = filePath;
+        videoPlayers[index].Prepare();
+        videoPlayers[index].loopPointReached += delegate {
             
         };
-        videoPlayer.prepareCompleted += delegate {
-            Debug.Log($"Get video size: {videoPlayer.texture.width}x{videoPlayer.texture.height}");
+        videoPlayers[index].prepareCompleted += delegate {
+            Debug.Log($"Get video size: {videoPlayers[index].texture.width}x{videoPlayers[index].texture.height}");
             
         };
 
         if(!imidiatePlay)
             return;
             
-        videoPlayer.Play();
+        videoPlayers[index].Play();
     }
 }
