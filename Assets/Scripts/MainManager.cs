4dede46560dae5873944a91bb5673424a4ca8857
nikki <nikki@gmail.com>
2023-01-31 15:24:16 +0800

    新播放格式

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -13,7 +13,8 @@ public class MainManager : MonoBehaviour
     public bool imidiatePlay = false;
     string _path;
 
-    void OnGUI() {
+    void OnGUI()
+    {
         var guiScale = new Vector3(Screen.width / 800.0f, Screen.height / 600.0f, 1.0f);
         GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, guiScale);
 
@@ -22,14 +23,16 @@ public class MainManager : MonoBehaviour
         GUILayout.Space(20);
         GUILayout.BeginVertical();
         // Open File Samples
-        if (GUILayout.Button("Open File Video 0")) {
-            var extensions = new [] {
+        if (GUILayout.Button("Open File Video (Wall)"))
+        {
+            var extensions = new[] {
                 new ExtensionFilter("Support Video Files", "mp4"),
                 new ExtensionFilter("All Files", "*" ),
             };
             var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
             WriteResult(result);
-            if(result.Length > 0){
+            if (result.Length > 0)
+            {
                 Debug.Log("Success");
                 SetupVideo(0, result[0]);
             }
@@ -37,14 +40,16 @@ public class MainManager : MonoBehaviour
 
         GUILayout.Space(20);
 
-        if (GUILayout.Button("Open File Video 1")) {
-            var extensions = new [] {
+        if (GUILayout.Button("Open File Video (Ground)"))
+        {
+            var extensions = new[] {
                 new ExtensionFilter("Support Video Files", "mp4"),
                 new ExtensionFilter("All Files", "*" ),
             };
             var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
             WriteResult(result);
-            if(result.Length > 0){
+            if (result.Length > 0)
+            {
                 Debug.Log("Success");
 
                 SetupVideo(1, result[0]);
@@ -53,82 +58,96 @@ public class MainManager : MonoBehaviour
 
         GUILayout.Space(20);
 
-        if (GUILayout.Button("Open File Video 2")) {
-            var extensions = new [] {
-                new ExtensionFilter("Support Video Files", "mp4"),
-                new ExtensionFilter("All Files", "*" ),
-            };
-            var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
-            WriteResult(result);
-            if(result.Length > 0){
-                Debug.Log("Success");
-
-                SetupVideo(2, result[0]);
-            }
+        if (GUILayout.Button("Sync Play"))
+        {
+            SyncPlay();
         }
 
         GUILayout.Space(20);
 
-        if (GUILayout.Button("Open File Video 3")) {
-            var extensions = new [] {
-                new ExtensionFilter("Support Video Files", "mp4"),
-                new ExtensionFilter("All Files", "*" ),
-            };
-            var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
-            WriteResult(result);
-            if(result.Length > 0){
-                Debug.Log("Success");
+        // if (GUILayout.Button("Open File Video 3"))
+        // {
+        //     var extensions = new[] {
+        //         new ExtensionFilter("Support Video Files", "mp4"),
+        //         new ExtensionFilter("All Files", "*" ),
+        //     };
+        //     var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+        //     WriteResult(result);
+        //     if (result.Length > 0)
+        //     {
+        //         Debug.Log("Success");
 
-                SetupVideo(3, result[0]);
-            }
-        }
+        //         SetupVideo(3, result[0]);
+        //     }
+        // }
 
-        GUILayout.Space(20);
+        // GUILayout.Space(20);
 
-        if (GUILayout.Button("Open File Video 4")) {
-            var extensions = new [] {
-                new ExtensionFilter("Support Video Files", "mp4"),
-                new ExtensionFilter("All Files", "*" ),
-            };
-            var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
-            WriteResult(result);
-            if(result.Length > 0){
-                Debug.Log("Success");
+        // if (GUILayout.Button("Open File Video 4"))
+        // {
+        //     var extensions = new[] {
+        //         new ExtensionFilter("Support Video Files", "mp4"),
+        //         new ExtensionFilter("All Files", "*" ),
+        //     };
+        //     var result = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+        //     WriteResult(result);
+        //     if (result.Length > 0)
+        //     {
+        //         Debug.Log("Success");
 
-                SetupVideo(4, result[0]);
-            }
-        }
+        //         SetupVideo(4, result[0]);
+        //     }
+        // }
 
         GUILayout.EndVertical();
         GUILayout.Space(20);
         GUILayout.Label(_path);
         GUILayout.EndHorizontal();
     }
-    public void WriteResult(string[] paths) {
-        if (paths.Length == 0) {
+    public void WriteResult(string[] paths)
+    {
+        if (paths.Length == 0)
+        {
             return;
         }
 
         _path = "";
-        foreach (var p in paths) {
+        foreach (var p in paths)
+        {
             _path += p + "\n";
         }
     }
 
-    void SetupVideo(int index, string filePath){
+    void SetupVideo(int index, string filePath)
+    {
         videoPlayers[index].url = filePath;
         videoPlayers[index].Prepare();
-        videoPlayers[index].loopPointReached += delegate {
-            
+        videoPlayers[index].loopPointReached += delegate
+        {
+
         };
-        videoPlayers[index].prepareCompleted += delegate {
+        videoPlayers[index].prepareCompleted += delegate
+        {
             Debug.Log($"Get video size: {videoPlayers[index].texture.width}x{videoPlayers[index].texture.height}");
-            
+
         };
 
-        if(!imidiatePlay)
+        if (!imidiatePlay)
             return;
-            
+
         videoPlayers[index].Play();
+    }
+
+    void SyncPlay()
+    {
+        if (videoPlayers[0].isPrepared && videoPlayers[1].isPrepared)
+        {
+            videoPlayers[0].Play();
+            videoPlayers[1].Play();
+        }
+        else
+        {
+            Debug.Log("not all video prepared!");
+        }
     }
 }
