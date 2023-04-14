042f8cd442b8de0a193b5e50bfb1fc326e303893
nikki <nikki@gmail.com>
2023-04-14 17:11:41 +0800

    更新VRTD模式

---
+++ Assets/Scripts/OSCTDIP.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using OscJack;
+
+public class OSCTDIP : MonoBehaviour
+{
+    public InputField INP_IP;
+    public OscConnection connect;
+    void Start()
+    {
+        INP_IP.onValueChanged.AddListener(x =>
+        {
+            connect.host = x;
+            SystemConfig.Instance.SaveData("OSCIP", x);
+        });
+        INP_IP.text = SystemConfig.Instance.GetData<string>("OSCIP", "192.168.1.13");
+    }
+}
